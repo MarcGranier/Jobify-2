da82bd525018bcb28c58035bd920fa1e6d1d804b
Marc Granier <mgranier@comcast.net>
2023-04-25 16:01:43 -0400

    Global Context Setup

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,18 +1,19 @@
 import { useState, useEffect } from 'react'
 import { Logo, FormRow, Alert } from '../components'
 import Wrapper from '../assets/wrappers/RegisterPage'
+import { useAppContext } from '../context/appContext'
 
 const initialState = {
 	name: '',
 	email: '',
 	password: '',
-	isMember: true,
-	showAlert: false
+	isMember: true
 }
 
 const Register = () => {
 	const [values, setValues] = useState(initialState)
 	//global state and useNavigate
+	const { isLoading, showAlert } = useAppContext()
 
 	const toggleMember = () => {
 		setValues({ ...values, isMember: !values.isMember })
@@ -30,7 +31,7 @@ const Register = () => {
 			<form className='form' onSubmit={onSubmit}>
 				<Logo />
 				<h3>{values.isMember ? 'Login' : 'Register'}</h3>
-				{values.showAlert && <Alert />}
+				{showAlert && <Alert />}
 				{/* name input */}
 				{!values.isMember && (
 					<FormRow
