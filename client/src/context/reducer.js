9df08fa87132b250fd139546118196d5b8035f28
Marc Granier <mgranier@comcast.net>
2023-04-26 17:43:44 -0400

    Clear Alert

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,4 +1,4 @@
-import { DISPLAY_ALERT } from './actions'
+import { DISPLAY_ALERT, CLEAR_ALERT } from './actions'
 
 const reducer = (state, action) => {
 	if (action.type === DISPLAY_ALERT) {
@@ -6,7 +6,15 @@ const reducer = (state, action) => {
 			...state,
 			showAlert: true,
 			alertType: 'danger',
-			alertText: 'please provide all values!'
+			alertText: 'Please provide all values!'
+		}
+	}
+	if (action.type === CLEAR_ALERT) {
+		return {
+			...state,
+			showAlert: false,
+			alertType: '',
+			alertText: ''
 		}
 	}
 	throw new Error(`no such action : ${action.type}`)
