c9577ebd26e187e42924bcb5aafadd23c3e0141f
Marc Granier <mgranier@comcast.net>
2023-04-27 12:03:50 -0400

    Connect to DB

---
+++ db/connect.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose'
+
+const connectDB = async (url) => {
+	return mongoose.connect(url)
+}
+export default connectDB
+
--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
 
-//middleware
+import connectDB from './db/connect.js'
 
+//middleware
 import notFoundMiddleware from './middleware/not-found.js'
 import errorHandlerMiddleware from './middleware/error-handler.js'
 
@@ -17,5 +18,17 @@ app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`))
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI)
+		console.log('MongoDB connected')
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}...`)
+		)
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+start()
 
