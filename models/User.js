8e55d23c4ef111503ffa36303e5682025bd60346
Marc Granier <mgranier@comcast.net>
2023-04-27 16:50:08 -0400

    User Model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'Please provide a name'],
+		minlength: 3,
+		maxlength: 20,
+		trim: true
+	},
+	email: {
+		type: String,
+		required: [true, 'Please provide a email'],
+		unique: true
+	},
+	password: {
+		type: String,
+		required: [true, 'Please provide a password'],
+		minlength: 6
+	},
+	lastName: {
+		type: String,
+		trim: true,
+		maxlength: 20,
+		default: 'lastName'
+	},
+	location: {
+		type: String,
+		trim: true,
+		maxlength: 20,
+		default: 'my city'
+	}
+})
+
+export default mongoose.model('User', UserSchema)
+
