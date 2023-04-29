fb3e5098227c1cf5a2e33ae093e3a32570faa3f1
Marc Granier <mgranier@comcast.net>
2023-04-29 14:29:51 -0400

    Remove Password

--- models/User.js
+++ models/User.js
@@ -23,7 +23,8 @@ const UserSchema = new mongoose.Schema({
 	password: {
 		type: String,
 		required: [true, 'Please provide a password'],
-		minlength: 6
+		minlength: 6,
+		select: false
 	},
 	lastName: {
 		type: String,
