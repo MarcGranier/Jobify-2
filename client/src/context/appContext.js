4e81a875d08501419ab42927722ff5e47a720e7b
Marc Granier <mgranier@comcast.net>
2023-05-03 18:34:17 -0400

    Login with Problems

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -8,7 +8,10 @@ import {
 	CLEAR_ALERT,
 	REGISTER_USER_BEGIN,
 	REGISTER_USER_SUCCESS,
-	REGISTER_USER_ERROR
+	REGISTER_USER_ERROR,
+	LOGIN_USER_BEGIN,
+	LOGIN_USER_SUCCESS,
+	LOGIN_USER_ERROR
 } from './actions'
 
 const token = localStorage.getItem('token')
@@ -76,8 +79,24 @@ const AppProvider = ({ children }) => {
 	}
 
 	const loginUser = async (currentUser) => {
-		console.log(currentUser)
+		dispatch({ type: LOGIN_USER_BEGIN })
+		try {
+			const { data } = await axios.post('/api/v1/auth/login', currentUser)
+			const { user, token, location } = data
+			dispatch({
+				type: LOGIN_USER_SUCCESS,
+				payload: { user, token, location }
+			})
+			addUserToLocalStorage({ user, token, location })
+		} catch (error) {
+			dispatch({
+				type: LOGIN_USER_ERROR,
+				payload: { msg: error.response.data.msg }
+			})
+		}
+		clearAlert()
 	}
+
 	return (
 		<AppContext.Provider
 			value={{ ...state, displayAlert, registerUser, loginUser }}
