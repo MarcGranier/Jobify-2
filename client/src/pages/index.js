39b22925e5e26cdcfa78f5e12bd85c1d1f347cd8
Marc Granier <mgranier@comcast.net>
2023-05-04 18:23:54 -0400

    Protected Route

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,7 @@
 import Error from './Error'
 import Landing from './Landing'
 import Register from './Register'
+import ProtectedRoute from './ProtectedRoute'
 
-export { Error, Landing, Register }
+export { Error, Landing, Register, ProtectedRoute }
+
