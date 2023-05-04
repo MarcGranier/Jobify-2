ff6ca9f21069217cbc1bce54f8e6265efe575057
Marc Granier <mgranier@comcast.net>
2023-05-04 17:53:57 -0400

    Dashboard Pages - Complete

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,5 +1,16 @@
+import { Outlet, Link } from 'react-router-dom'
+import Wrapper from '../../assets/wrappers/SharedLayout'
+
 const SharedLayout = () => {
-	return <div>SharedLayout Page</div>
+	return (
+		<Wrapper>
+			<nav>
+				<Link to='add-job'>Add Job</Link>
+				<Link to='all-jobs'>All Jobs</Link>
+			</nav>
+			<Outlet />
+		</Wrapper>
+	)
 }
 
 export default SharedLayout
