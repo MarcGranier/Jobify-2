53008d32df55d5596d5ecd65f737f37d5b00ec7a
Marc Granier <mgranier@comcast.net>
2023-05-05 17:23:24 -0400

    React Icons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import Wrapper from '../assets/wrappers/Navbar'
+import { FaHome } from 'react-icons/fa'
 
 const Navbar = () => {
 	return (
 		<Wrapper>
 			<h4>navbar</h4>
+			<FaHome />
 		</Wrapper>
 	)
 }
