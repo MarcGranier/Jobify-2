e9bc29ef13edbb28151308f1e8346117806fb5ab
Marc Granier <mgranier@comcast.net>
2023-05-06 17:43:26 -0400

    Navbar Structure Complete

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,44 @@
 import Wrapper from '../assets/wrappers/Navbar'
-import { FaHome } from 'react-icons/fa'
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa'
+import { useAppContext } from '../context/appContext'
+import Logo from './Logo'
 
 const Navbar = () => {
 	return (
 		<Wrapper>
-			<h4>navbar</h4>
-			<FaHome />
+			<div className='nav-center'>
+				<button
+					type='button'
+					className='toggle-btn'
+					onClick={() => console.log('toggle sidebar')}
+				>
+					<FaAlignLeft />
+				</button>
+				<div>
+					<Logo />
+					<h3 className='logo-text'>dashboard</h3>
+				</div>
+				<div className='btn-container'>
+					<button
+						type='button'
+						className='btn'
+						onClick={() => console.log('show.hide dropdown')}
+					>
+						<FaUserCircle />
+						john
+						<FaCaretDown />
+					</button>
+					<div className='dropdown show-dropdown'>
+						<button
+							type='button'
+							className='dropdown-btn'
+							onClick={() => console.log('logout user')}
+						>
+							logout
+						</button>
+					</div>
+				</div>
+			</div>
 		</Wrapper>
 	)
 }
