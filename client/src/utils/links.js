d52007c72f86666a3182f0e5e180513a20413ee0
Marc Granier <mgranier@comcast.net>
2023-05-08 11:43:09 -0400

    Setup Links

---
+++ client/src/utils/links.js
@@ -0,0 +1,12 @@
+import { IoBarChartSharp } from 'react-icons/io5'
+import { MdQueryStats } from 'react-icons/md'
+import { FaWpforms } from 'react-icons/fa'
+import { ImProfile } from 'react-icons/im'
+
+const links = [{ id: 1, text: 'stats', path: '/', icon: <IoBarChartSharp /> }],
+const links = [{ id: 2, text: 'all jobs', path: 'all-jobs', icon:  <MdQueryStats /> }],
+const links = [{ id: 3, text: 'add job', path: 'add-job', icon: <FaWpforms /> }],
+const links = [{ id: 4, text: 'profile', path: 'profile', icon: <ImProfile /> }]
+
+
+export default links
