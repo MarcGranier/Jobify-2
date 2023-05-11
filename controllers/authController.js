1d801c1cb5c6af8f59206896ea1443eb588cde9e
Marc Granier <mgranier@comcast.net>
2023-05-11 12:40:36 -0400

    Update User

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,20 @@
-import User from '../models/User.js'
-import { StatusCodes } from 'http-status-codes'
-import { BadRequestError, UnAuthenticatedError } from '../errors/index.js'
+import User from '../models/User.js';
+import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, UnAuthenticatedError } from '../errors/index.js';
 
 const register = async (req, res) => {
-	const { name, email, password } = req.body
+	const { name, email, password } = req.body;
 
 	if (!name || !email || !password) {
-		throw new BadRequestError('please provide all values')
+		throw new BadRequestError('please provide all values');
 	}
 
-	const userAlreadyExists = await User.findOne({ email })
+	const userAlreadyExists = await User.findOne({ email });
 	if (userAlreadyExists) {
-		throw new BadRequestError('Email already in use')
+		throw new BadRequestError('Email already in use');
 	}
-	const user = await User.create({ name, email, password })
-	const token = user.createJWT()
+	const user = await User.create({ name, email, password });
+	const token = user.createJWT();
 	res.status(StatusCodes.CREATED).json({
 		user: {
 			email: user.email,
@@ -24,28 +24,46 @@ const register = async (req, res) => {
 		},
 		token,
 		location: user.location
-	})
-}
+	});
+};
 const login = async (req, res) => {
-	const { email, password } = req.body
+	const { email, password } = req.body;
 	if (!email || !password) {
-		throw new BadRequestError('Please provide all values')
+		throw new BadRequestError('Please provide all values');
 	}
-	const user = await User.findOne({ email }).select('+password')
+	const user = await User.findOne({ email }).select('+password');
 	if (!user) {
-		throw new UnAuthenticatedError('Invalid Credentials')
+		throw new UnAuthenticatedError('Invalid Credentials');
 	}
-	const isPasswordCorrect = await user.comparePassword(password)
+	const isPasswordCorrect = await user.comparePassword(password);
 	if (!isPasswordCorrect) {
-		throw new UnAuthenticatedError('Invalid Credentials')
+		throw new UnAuthenticatedError('Invalid Credentials');
 	}
-	const token = user.createJWT()
-	user.password = undefined
-	res.status(StatusCodes.OK).json({ user, token, location: user.location })
-}
+	const token = user.createJWT();
+	user.password = undefined;
+	res.status(StatusCodes.OK).json({ user, token, location: user.location });
+};
 const updateUser = async (req, res) => {
-	res.send('updateUser')
-}
+	const { email, name, lastName, location } = req.body;
+	if (!email || !name || !lastName || !location) {
+		throw new BadRequestError('Please provide all values');
+	}
 
-export { register, login, updateUser }
+	const user = await User.findOne({ _id: req.user.userId });
 
+	user.email = email;
+	user.name = name;
+	user.lastName = lastName;
+	user.location = location;
+
+	await user.save();
+
+	const token = user.createJWT();
+	res.status(StatusCodes.OK).json({
+		user,
+		token,
+		location: user.location
+	});
+};
+
+export { register, login, updateUser };
--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
-import mongoose from 'mongoose'
-import validator from 'validator'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new mongoose.Schema({
 	name: {
@@ -38,24 +38,22 @@ const UserSchema = new mongoose.Schema({
 		maxlength: 20,
 		default: 'my city'
 	}
-})
+});
 
 UserSchema.pre('save', async function () {
-	const salt = await bcrypt.genSalt(10)
-	this.password = await bcrypt.hash(this.password, salt)
-})
+	// const salt = await bcrypt.genSalt(10);
+	// this.password = await bcrypt.hash(this.password, salt);
+});
 
 UserSchema.methods.createJWT = function () {
 	return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
-		// expiresIn: process.env.JWT_LIFETIME
-		expiresIn: '100', 
-	})
-}
+		expiresIn: process.env.JWT_LIFETIME
+	});
+};
 
 UserSchema.methods.comparePassword = async function (candidatePassword) {
-	const isMatch = await bcrypt.compare(candidatePassword, this.password)
-	return isMatch
-}
+	const isMatch = await bcrypt.compare(candidatePassword, this.password);
+	return isMatch;
+};
 
-export default mongoose.model('User', UserSchema)
-
+export default mongoose.model('User', UserSchema);
