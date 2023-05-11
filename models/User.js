cbecab12f97bb783b49fb1cc9a9abdc7f9a114ed
Marc Granier <mgranier@comcast.net>
2023-05-11 13:03:12 -0400

    Modified Paths

--- models/User.js
+++ models/User.js
@@ -41,8 +41,12 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre('save', async function () {
-	// const salt = await bcrypt.genSalt(10);
-	// this.password = await bcrypt.hash(this.password, salt);
+	// console.log(this.modifiedPaths());
+	// console.log(this.isModified('name'));
+	if (!this.isModified('password')) return;
+	// if (this.isModified('password')) return;
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
 });
 
 UserSchema.methods.createJWT = function () {
