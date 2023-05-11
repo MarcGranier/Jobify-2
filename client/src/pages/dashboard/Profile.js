a25771023c2756e6da30556298101d26be34facf
Marc Granier <mgranier@comcast.net>
2023-05-11 17:55:16 -0400

    Profile Page

--- client/src/pages/dashboard/Profile.js
+++ client/src/pages/dashboard/Profile.js
@@ -1,6 +1,20 @@
+import { useState } from 'react';
+import { FormRow, Alert } from '../../components';
+import { useAppContext } from '../../context/appContext';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+
 const Profile = () => {
-	return <h1>ProfilePage</h1>
-}
+	const { user, showAlert, displayAlert, updateUser, isLoading } =
+		useAppContext();
 
-export default Profile
+	const [name, setName] = useState(user?.name);
+	const [email, setEmail] = useState(user?.email);
+	const [lastName, setlastName] = useState(user?.lastName);
+	const [location, setlocation] = useState(user?.location);
 
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('update user');
+	};
+};
+export default Profile;
