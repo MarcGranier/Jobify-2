c098d10dd050b52ff7b27897901b9870b51a25ce
Marc Granier <mgranier@comcast.net>
2023-05-13 17:11:22 -0400

    Update User Complete

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -5,7 +5,10 @@ import {
 	SETUP_USER_SUCCESS,
 	SETUP_USER_ERROR,
 	TOGGLE_SIDEBAR,
-	LOGOUT_USER
+	LOGOUT_USER,
+	UPDATE_USER_BEGIN,
+	UPDATE_USER_SUCCESS,
+	UPDATE_USER_ERROR,
 } from './actions'
 
 import { initialState } from './appContext'
@@ -16,7 +19,7 @@ const reducer = (state, action) => {
 			...state,
 			showAlert: true,
 			alertType: 'danger',
-			alertText: 'Please provide all values!'
+			alertText: 'Please provide all values!',
 		}
 	}
 	if (action.type === CLEAR_ALERT) {
@@ -24,7 +27,7 @@ const reducer = (state, action) => {
 			...state,
 			showAlert: false,
 			alertType: '',
-			alertText: ''
+			alertText: '',
 		}
 	}
 	if (action.type === SETUP_USER_BEGIN) {
@@ -40,7 +43,7 @@ const reducer = (state, action) => {
 			jobLocation: action.payload.location,
 			showAlert: true,
 			alertType: 'success',
-			alertText: action.payload.alertText
+			alertText: action.payload.alertText,
 		}
 	}
 	if (action.type === SETUP_USER_ERROR) {
@@ -49,13 +52,13 @@ const reducer = (state, action) => {
 			isLoading: false,
 			showAlert: true,
 			alertType: 'danger',
-			alertText: action.payload.msg
+			alertText: action.payload.msg,
 		}
 	}
 	if (action.type === TOGGLE_SIDEBAR) {
 		return {
 			...state,
-			showSidebar: !state.showSidebar
+			showSidebar: !state.showSidebar,
 		}
 	}
 	if (action.type === LOGOUT_USER) {
@@ -64,11 +67,34 @@ const reducer = (state, action) => {
 			user: null,
 			token: null,
 			jobLocation: '',
-			userLocation: ''
+			userLocation: '',
 		}
 	}
-
+	if (action.type === UPDATE_USER_BEGIN) {
+		return { ...state, isLoading: true }
+	}
+	if (action.type === UPDATE_USER_SUCCESS) {
+		return {
+			...state,
+			isLoading: false,
+			token: action.payload.token,
+			user: action.payload.user,
+			userLocation: action.payload.location,
+			jobLocation: action.payload.location,
+			showAlert: true,
+			alertType: 'success',
+			alertText: 'User Profile Updated!',
+		}
+	}
+	if (action.type === UPDATE_USER_ERROR) {
+		return {
+			...state,
+			isLoading: false,
+			showAlert: true,
+			alertType: 'danger',
+			alertText: action.payload.msg,
+		}
+	}
 	throw new Error(`no such action : ${action.type}`)
 }
 export default reducer
-
