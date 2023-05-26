e9806bf8d7731fd7e093abb000c4127b6c31841a
Marc Granier <mgranier@comcast.net>
2023-05-26 17:29:46 -0400

    Get All Jobs Controller(Again)

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -7,17 +7,21 @@ const createJob = async (req, res) => {
 
 	if (!position || !company) {
 		throw new BadRequestError('Please provide position and company')
-	}
-	req.body.createdBy = req.user.userId
-	const job = await Job.create(req.body)
-	res.status(StatusCodes.CREATED).json({job})
-}
-const getAllJobs = async (res,req) =>{
-	const jobs = await Job.find({createdBy:req.user.userId})
-	res
-		.status(StatusCodes.OK)
-		.json({jobs, totalJobs:jobs.length, numOfPages:1})	
-}
+  }
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+};
+const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({ createdBy: req.user.userId });
+
+  res
+    .status(StatusCodes.OK)
+    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
+		console.log(req.user.userId);	
+};
+
+
 const updateJob = async (req, res) => {
 	res.send('updateJob')
 }
