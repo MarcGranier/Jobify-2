ac0a9b6e2982b82f0c919b2e9056edb922864d0b
Marc Granier <mgranier@comcast.net>
2023-06-05 13:14:38 -0400

    Reduce Stats

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -65,6 +65,11 @@ const showStats = async (req,res) => {
     {$match:{createdBy:new mongoose.Types.ObjectId(req.user.userId)}},
     {$group: { _id: '$status', count: {$sum: 1} } },
   ]) 
+  stats = stats.reduce((acc, curr) =>{
+    const {_id:title,count} = curr
+    acc[title] = count
+    return acc
+  },{})
   res.status(StatusCodes.OK).json({ stats})
 }
 
