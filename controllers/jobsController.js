cabe6eae7e3804ad8e5e9c377807c0d7bb049bbf
Marc Granier <mgranier@comcast.net>
2023-06-05 13:28:21 -0400

    Default Stats

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -70,7 +70,14 @@ const showStats = async (req,res) => {
     acc[title] = count
     return acc
   },{})
-  res.status(StatusCodes.OK).json({ stats})
+  
+  const defaultStats = {
+    pending:stats.pending || 0,
+    interview:stats.interview || 0,
+    declined:stats.declined || 0,
+  }
+  let monthlyApplications = []
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications})
 }
 
 
