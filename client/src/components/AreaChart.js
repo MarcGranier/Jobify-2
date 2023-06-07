45830b981944159207abd9b709a8ea28a60c65bd
Marc Granier <mgranier@comcast.net>
2023-06-07 18:08:52 -0400

    Area Chart

--- client/src/components/AreaChart.js
+++ client/src/components/AreaChart.js
@@ -1,9 +1,25 @@
-const AreaChart = () => {
+import {
+	ResponsiveContainer,
+	AreaChart,
+	Area,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+} from 'recharts'
+
+const AreaChartComponent = ({ data }) => {
 	return (
-		<div>
-			<h2>AreaChart</h2>
-		</div>
+		<ResponsiveContainer width='100%' height={300}>
+			<AreaChart data={data} margin={{ top: 50 }}>
+				<CartesianGrid strokeDasharray='3 3' />
+				<XAxis dataKey='date' />
+				<YAxis allowDecimals={false} />
+				<Tooltip />
+				<Area type='monotone' dataKey='count' stroke='#2cb1bc' fill='#03d3fc' />
+			</AreaChart>
+		</ResponsiveContainer>
 	)
 }
 
-export default AreaChart
+export default AreaChartComponent
