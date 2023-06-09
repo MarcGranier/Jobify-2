8348aa369a40271797215f7291632d72ad2ae76d
Marc Granier <mgranier@comcast.net>
2023-06-09 16:14:15 -0400

    Query Parameter - Sort

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -41,6 +41,18 @@ const getAllJobs = async (req, res) => {
 
 	// chain sort conditions
 
+	if (sort === 'latest') {
+		result = result.sort('-createdAt')
+	}
+	if (sort === 'oldest') {
+		result = result.sort('createdAt')
+	}
+	if (sort === 'a-z') {
+		result = result.sort('position')
+	}
+	if (sort === 'z-a') {
+		result = result.sort('-position')
+	}
 	const jobs = await result
 
 	res
