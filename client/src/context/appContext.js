b955096f2e23fc5695365f76e53a3dc938789f03
Marc Granier <mgranier@comcast.net>
2023-06-11 18:40:22 -0400

    Chnage Page

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -29,6 +29,7 @@ import {
 	SHOW_STATS_BEGIN,
 	SHOW_STATS_SUCCESS,
 	CLEAR_FILTERS,
+	CHANGE_PAGE,
 } from './actions'
 
 const token = localStorage.getItem('token')
@@ -285,6 +286,11 @@ const AppProvider = ({ children }) => {
 	const clearFilters = () => {
 		dispatch({ type: CLEAR_FILTERS })
 	}
+
+	const changePage = (page) => {
+		dispatch({ type: CHANGE_PAGE, payload: { page } })
+	}
+
 	return (
 		<AppContext.Provider
 			value={{
@@ -303,6 +309,7 @@ const AppProvider = ({ children }) => {
 				editJob,
 				showStats,
 				clearFilters,
+				changePage,
 			}}
 		>
 			{children}
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -24,6 +24,7 @@ import {
 	SHOW_STATS_BEGIN,
 	SHOW_STATS_SUCCESS,
 	CLEAR_FILTERS,
+	CHANGE_PAGE,
 } from './actions'
 
 import { initialState } from './appContext'
@@ -229,7 +230,9 @@ const reducer = (state, action) => {
 			sort: 'latest',
 		}
 	}
-
+	if (action.type === CHANGE_PAGE) {
+		return { ...state, page: action.payload.page }
+	}
 	throw new Error(`no such action : ${action.type}`)
 }
 
