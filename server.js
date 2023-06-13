d225b6481c836869a998b7add0d364873afefc46
Marc Granier <mgranier@comcast.net>
2023-06-13 11:14:51 -0400

    Security Package - Express Rate Limiter

--- server.js
+++ server.js
@@ -38,6 +38,9 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.use(express.static(path.resolve(__dirname, './client/build')))
 app.use(express.json())
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
