b93abd32833d05e4cd856584d61cf4938036c5b0
Marc Granier <mgranier@comcast.net>
2023-06-13 16:03:23 -0400

    Debounce Complete

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,8 +1,10 @@
 import { FormRow, FormRowSelect } from '.'
 import { useAppContext } from '../context/appContext'
 import Wrapper from '../assets/wrappers/SearchContainer'
+import { useState, useMemo } from 'react'
 
 const SearchContainer = () => {
+	const [localSearch, setLocalSearch] = useState('')
 	const {
 		isLoading,
 		search,
@@ -17,25 +19,40 @@ const SearchContainer = () => {
 	} = useAppContext()
 
 	const handleSearch = (e) => {
-		if (isLoading) return
 		handleChange({ name: e.target.name, value: e.target.value })
+	}
+
+	const debounce = () => {
+		let timeoutID
+		return (e) => {
+			setLocalSearch(e.target.value)
+			clearTimeout(timeoutID)
+			timeoutID = setTimeout(() => {
+				handleChange({ name: e.target.name, value: e.target.value })
+			}, 1000)
+		}
 	}
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
+		setLocalSearch('')
 		clearFilters()
 	}
+
+	const optimizedDebounce = useMemo(() => debounce(), [])
+
 	return (
 		<Wrapper>
 			<form className='form'>
 				<h4>search form</h4>
-				{/* search position */}
 				<div className='form-center'>
+					{/* search position */}
 					<FormRow
 						type='text'
 						name='search'
-						value={search}
-						handleChange={handleSearch}
+						value={localSearch}
+						handleChange={optimizedDebounce}
+					/>
 					/>
 					{/* search by status */}
 					<FormRowSelect
