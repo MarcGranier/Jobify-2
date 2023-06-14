33ada2a096779eade88f3cea4c479a3fe9ef82ec
Marc Granier <mgranier@comcast.net>
2023-06-14 12:02:18 -0400

    First Cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,7 +41,14 @@ const login = async (req, res) => {
 		throw new UnauthenticatedError('Invalid Credentials')
 	}
 	const token = user.createJWT()
+	const oneDay = 1000 * 60 * 60 * 24
 	user.password = undefined
+
+	res.cookie('token', token, {
+		httpOnly: true,
+		expires: new Date(Date.now() + oneDay),
+		secure: process.env.NODE_ENV,
+	})
 	res.status(StatusCodes.OK).json({ user, token, location: user.location })
 }
 const updateUser = async (req, res) => {
